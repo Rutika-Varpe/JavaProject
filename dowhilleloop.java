074326e6123b285eca9f6aa6da4082867bd1c9df
RutikaVarpe <rutikavarpe@gmail.com>
2025-01-08 19:51:59 +0530

    Do-while loop

---
+++ dowhilleloop.java
@@ -0,0 +1,13 @@
+public class dowhilleloop 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Natural numbers between 1 to 10 : ");
+        int i = 1;
+        do
+        {
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+    }
+}
