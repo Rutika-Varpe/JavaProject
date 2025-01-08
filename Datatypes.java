e277d59c5f27526ab22ea3e5fca9982079dd1eaa
RutikaVarpe <rutikavarpe@gmail.com>
2025-01-08 15:03:03 +0530

    Datatypes in java

---
+++ Datatypes.java
@@ -0,0 +1,24 @@
+public class Datatypes 
+{
+    public static void main(String[] args) 
+    {
+        int num =100 ;
+        byte by = 1;
+        short s = 2;
+        long l = 123l;
+        float f = 12.5f;
+        double d = 1123;
+        char c ='c';
+        boolean b = true;
+
+        System.out.println("int : "+num);
+        System.out.println("byte : "+by);
+        System.out.println("short : "+s);
+        System.out.println("long : "+l);
+        System.out.println("float : "+f);
+        System.out.println("double : "+d);
+        System.out.println("char : "+c);
+        System.out.println("boolean : "+b);
+
+    }
+}
