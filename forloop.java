0ca4323ca34adbbd1468ebbc47e0c784171cf5b7
RutikaVarpe <rutikavarpe@gmail.com>
2025-01-08 19:38:35 +0530

    While loop

--- forloop.java
+++ forloop.java
@@ -2,11 +2,11 @@ public class forloop
 {
     public static void main(String[] args) {
         
-
-        for(int i = 1;i<=10;i++)
+        int n = 10;
+        for(int i = 1;i<=n;i++)
         {
             System.out.println(i);
-            
+
         }
     }
 }
