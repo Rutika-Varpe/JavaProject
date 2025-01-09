250fb05791242facbf75c4ad42f074b3e1369bd6
RutikaVarpe <rutikavarpe@gmail.com>
2025-01-09 12:17:12 +0530

    Type casting

---
+++ typecasting.java
@@ -0,0 +1,19 @@
+public class typecasting 
+{
+    public static void main(String[] args) 
+    {
+        // type casting
+        // 1 = automatic 
+        byte b = 10;
+        int a = b;
+        System.out.println(a);
+
+        // type casting
+        // 2 = explicit
+        
+        float f = 20.5f;
+        int c = (int)f;
+
+        System.out.println(c);
+    }
+}
